a1abb0db8890ccaad15119aa3ba4a7f4074bb636
Oscar Corea <ocorearojas@gmail.com>
2021-11-12 20:58:10 +0100

    Fixed Pipe DateTimeAgo to avoid errors such as '1 hours ago'

--- src/app/pipes/date-time-ago.pipe.ts
+++ src/app/pipes/date-time-ago.pipe.ts
@@ -5,21 +5,31 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class DateTimeAgoPipe implements PipeTransform {
 
+  private _intervals = [
+    { divisor: 31536000, unit: "year" },
+    { divisor: 2592000, unit: "month" },
+    { divisor: 86400, unit: "day" },
+    { divisor: 3600, unit: "hour" },
+    { divisor: 60, unit: "minute" },
+  ]
+
   transform(dateString: string): string {
     let date = new Date(dateString);
     let seconds = Math.floor((new Date().getTime() - date.getTime()) / 1000);
-    let interval = seconds / 31536000;
-  
-    if (interval > 1) { return Math.floor(interval) + " years ago"; }
-    interval = seconds / 2592000;
-    if (interval > 1) { return Math.floor(interval) + " months ago"; }
-    interval = seconds / 86400;
-    if (interval > 1) { return Math.floor(interval) + " days ago"; }
-    interval = seconds / 3600;
-    if (interval > 1) { return Math.floor(interval) + " hours ago"; }
-    interval = seconds / 60;
-    if (interval > 1) { return Math.floor(interval) + " minutes ago"; }
-    return Math.floor(seconds) + " seconds ago";
-  }
+    let unit = "second";
+    let intervalReturn = Math.floor(seconds);
 
+    for (let i = 0 ; i < this._intervals.length ; i++) {
+      const intervalObject = this._intervals[i];
+      const interval = seconds / intervalObject.divisor;
+      
+      if (interval > 1) {
+        intervalReturn = Math.floor(interval);
+        unit = intervalObject.unit;
+        break;
+      }
+    }
+
+    return `${ intervalReturn } ${ unit }${ intervalReturn > 1 ? "s" : "" } ago`;
+  }
 }
